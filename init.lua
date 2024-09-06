83ce108acdb258b830f190fe13318cd7679f6e9a
veryslowcode <veryslowcode@gmail.com>
2024-09-05 21:26:19 -0600

    Updated lualine to have additional information on inactive sections.

--- init.lua
+++ init.lua
@@ -179,7 +179,16 @@ require("lazy").setup({
         config = function()
             require("lualine").setup {
                 options = {
-                    icons_enabled = false
+                    icons_enabled = false,
+                    always_divide_middle = true,
+                },
+                inactive_sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {'filename'},
+                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
                 }
             }
         end
